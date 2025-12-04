546d88db4dd927c48d1b179b708c5c299ff5477b
MattChew369 <ciumatthew08@gmail.com>
2025-12-04 11:47:42 -0500

    game.py

--- app/game.py
+++ app/game.py
@@ -3,3 +3,21 @@
 # SoftDev
 # P01: ArRESTed Development
 # Dec 2025
+
+
+
+class pokemon:
+    id: int
+    health: int
+
+
+class player:
+    id: int
+    list: list # list of pokemon on their team
+    poke_out: int
+
+    def action(poke_out, move):
+        
+
+def pokemon():
+    p1 = player()
