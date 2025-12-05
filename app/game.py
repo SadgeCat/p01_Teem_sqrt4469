bc3efb3c0f55fb0173041a03c04115fc32c99306
MattChew369 <ciumatthew08@gmail.com>
2025-12-05 11:47:36 -0500

    random generator chugging'

--- app/game.py
+++ app/game.py
@@ -3,13 +3,17 @@
 # SoftDev
 # P01: ArRESTed Development
 # Dec 2025
+import random
+from apis import get_pokemon
+from apis import get_superhero
 
-
+url = "https://pokeapi.co/api/v2/pokemon/"
 
 class pokemon:
     id: int
+    skin: str
     health: int
-
+    pic: str
 
 class player:
     id: int
@@ -17,7 +21,26 @@ class player:
     poke_out: int
 
     def action(poke_out, move):
-        
+        get_pokemon()
 
-def pokemon():
+def intialize():
     p1 = player()
+    p1.id = 1
+
+
+    p2 = player()
+
+def random_poke(player):
+    x = 0
+    while x <= 5:
+        # create pokemon obj WHERE
+        id = random.randomint(1, 1025)
+        getpokemon(id)
+        #create adding to list here
+        player.list[x] =
+
+
+    get_pokemon()
+# return pokemon
+
+print(get_pokemon(150))
