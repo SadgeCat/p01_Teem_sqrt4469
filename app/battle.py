3de64f42db68dc4b337957f98b98901c73428014
Thomas Mackey <thomasmackey99@gmail.com>
2025-12-07 21:57:10 -0500

    added battle.py for game mechanics

---
+++ app/battle.py
@@ -0,0 +1,104 @@
+import random
+from apis import get_superhero, get_anime_character
+
+# All charatcers will share the same 3 moves for now
+MOVES = {
+    "strike": {
+        "name": "Strike",
+        "power": 20,
+        "description": "A basic, reliable attack."
+    },
+    "power_blast": {
+        "name": "Power Blast",
+        "power": 30,
+        "description": "Stronger hit."
+    },
+    "quick_hit": {
+        "name": "Quick Hit",
+        "power": 15,
+        "description": "Weaker hit."
+    }
+}
+
+
+# ------------- FIGHTER CREATION -------------
+
+def make_random_fighter():
+    source = random.choice(["superhero", "anime"])
+
+    if source == "superhero":
+        base = get_superhero(0)     r
+    else:
+        base = get_anime_character(0)
+
+    fighter = {
+        "name": base["name"],
+        "image": base["image"],
+        "hp": int(base["hp"]),
+        "current_hp": int(base["hp"]),
+        "atk": int(base["atk"]),
+        "def": int(base["def"]),
+        "speed": int(base["speed"]),
+        "source": source
+    }
+    return fighter
+
+
+def create_game_state():
+
+    p1 = make_random_fighter()
+    p2 = make_random_fighter()
+
+    if p1["speed"] >= p2["speed"]:
+        first = "p1"
+    else:
+        first = "p2"
+
+    state = {
+        "p1": p1,
+        "p2": p2,
+        "turn": first,
+        "log": [f"{p1['name']} vs {p2['name']}! {first.upper()} goes first."],
+        "winner": None
+    }
+    return state
+
+
+
+def apply_move(state, move_key):
+
+    if state["winner"] is not None:
+        return state
+
+    attacker_key = state["turn"]
+    defender_key = "p1" if attacker_key == "p2" else "p2"
+
+    attacker = state[attacker_key]
+    defender = state[defender_key]
+
+    move = MOVES[move_key]
+    power = move["power"]
+
+    # Damage formula:
+    # damage = move power + small part of attacker's atk - small part of defender's def
+    damage = power + attacker["atk"] // 10 - defender["def"] // 20
+
+    if damage < 1:
+        damage = 1
+
+    defender["current_hp"] -= damage
+    if defender["current_hp"] < 0:
+        defender["current_hp"] = 0
+
+    message = f"{attacker['name']} used {move['name']} and dealt {damage} damage."
+    state["log"].append(message)
+
+    # Check if defender is defeated
+    if defender["current_hp"] == 0:
+        state["winner"] = attacker_key
+        state["log"].append(f"{defender['name']} is defeated!")
+    else:
+        # Switch turns if no one has won yet
+        state["turn"] = defender_key
+
+    return state
